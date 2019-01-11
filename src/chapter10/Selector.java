bfb7e266ac4850fc88246a54a9ec0a439bb7950f
韩小明同学 <handm2018@126.com>
2019-01-11 18:00:58 +0800

    接口selector添加注释

--- src/chapter10/Selector.java
+++ src/chapter10/Selector.java
@@ -1,15 +1,16 @@
 package chapter10;
-/** 
- * ClassName: Select <br/> 
- * Function: TODO ADD FUNCTION. <br/> 
- * Reason: TODO ADD REASON(可选). <br/> 
- * date: 2019年1月11日 下午3:30:07 <br/> 
- * 
- * @author handm2018@126.com 
- * @version  
+
+/**
+ * ClassName: Select <br/>
+ * Function: 接口规约，使用于squence类中的内部类（sequence的迭代器），. <br/>
+ * date: 2019年1月11日 下午3:30:07 <br/>
+ * @author handm2018@126.com
+ * @version
  */
 public interface Selector {
     boolean end();
+
     Object cuurent();
+
     void next();
 }
