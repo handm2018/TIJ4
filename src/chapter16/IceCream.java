8a260b7fbcd85b53c51ba500f5ec1ee7aae340b8
handm2018@126.com <handm2018@126.com>
2019-04-15 22:51:41 +0800

    返回数组注释

--- src/chapter16/IceCream.java
+++ src/chapter16/IceCream.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * @author handm2018@126.com
  * @version 1.0.0
  * @ClassName IceCream.java
- * @Description TODO
+ * @Description 返回数组
  * @createTime 2019年04月15日 15:25:00
  */
 public class IceCream {
@@ -19,22 +19,29 @@ public class IceCream {
 
     public static void main(String[] args) {
         for (int i = 0; i < 8; i++) {
-            System.out.println(Arrays.toString(favorsSet(4)));
+            System.out.println(Arrays.toString(favorsSet(6)));
         }
     }
 
     public static String[] favorsSet(int n) {
+
+        // 保证数组中的口味不会重复
         if (n > FLAVORS.length) {
             throw new IllegalArgumentException("set too big");
         }
+
         String[] results = new String[n];
+        // 该位置的口味是否已被选择
         boolean[] picked = new boolean[FLAVORS.length];
         for (int i = 0; i < n; i++) {
+            //临时变量t，随机选择口味
             int t;
             do {
                 t = random.nextInt(FLAVORS.length);
-            } while (picked[t]);
+            } while (picked[t]); //若该口味已被选择，重新选择
+            //将随机选择的口味放到数组中
             results[i] = FLAVORS[t];
+            //将选择的此口味标记为已选择
             picked[t] = true;
         }
         return results;
