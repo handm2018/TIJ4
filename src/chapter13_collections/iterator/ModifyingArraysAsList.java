90998cef573a5551d9aa4ce2495f088c34963de7
handongming <handm2018@126.com>
2020-05-15 09:52:57 +0800

    集合系列-队列，Arrays.asList()

--- src/chapter13_collections/iterator/ModifyingArraysAsList.java
+++ src/chapter13_collections/iterator/ModifyingArraysAsList.java
@@ -1,5 +1,7 @@
 package chapter13_collections.iterator;
 
+import java.util.*;
+
 /**
  *@program: TIJ4
  *@description: Arrays.asList()
@@ -9,7 +11,19 @@ package chapter13_collections.iterator;
 public class ModifyingArraysAsList {
 
     public static void main(String[] args) {
+        Random random = new Random(47);
+        Integer[] ia = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(ia));
+        System.out.println("Before shuffling: " + list1);
+        Collections.shuffle(list1,random);
+        System.out.println("After shuffling: " + list1);
+        System.out.println("array: " + Arrays.toString(ia));
 
+        List<Integer> list2 = Arrays.asList(ia);
+        System.out.println("Before shuffling: " + list2);
+        Collections.shuffle(list2,random);
+        System.out.println("After shuffling: " + list2);
+        System.out.println("array: " + Arrays.toString(ia));
     }
 
 }
